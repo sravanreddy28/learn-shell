41eda4978725c2b30f75d93ae2a4ddb61da3fb3f
sravan k <sravankakireddy@gmail.com>
2023-07-25 11:18:28 +0530

    fun1

---
+++ function.sh
@@ -0,0 +1,7 @@
+HELLO!!!(){
+  echo how you doing
+  echo how is training experience
+  echo are you happy
+  echo Best of luck for future endevours
+}
+HELLO!!!
--- variable.sh
+++ variable.sh
@@ -5,12 +5,3 @@ echo today date is $DATE
 #Arthimetic substitution
 add=$((2+2))
 echo add 2+2 =$add
-
-#name substitution
-name=$(sravan)
-age=$25
-role=(analyst)
-
-echo your name sravan $name
-echo your $age
-echo your position $role
