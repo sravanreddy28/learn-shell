bd6797e4ad75ffa7ab8cfeed3195e9eda0a1c88a
sravan k <sravankakireddy@gmail.com>
2023-07-25 11:25:59 +0530

    FUNC 4

--- function.sh
+++ function.sh
@@ -2,8 +2,8 @@ HELLO!!!(){
   echo how you doing
   echo how is your training experience
   echo Are you happy
-  return 20
   echo Best of luck for your future endevours
+  return 20
 }
 HELLO!!!
 echo see you next time BYE!!! - $?
